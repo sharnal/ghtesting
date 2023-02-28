e80d13b96320832f4d9f2369bb19729cc0e2b26d
Shubham Harnal <shubham.harnal@live.com>
2023-03-01 03:27:08 +0530

    Reset for testing

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -20,5 +20,6 @@ describe('MainContent', () => {
     const button = screen.getByRole('button');
     await userEvent.click(button);
     expect(screen.getByTestId('help-area')).toBeInTheDocument();
+
   });
 });
